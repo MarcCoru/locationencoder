3a37d50d1a4e6120472fd152d81510e3f296c151
Marc Rußwurm <marc.russwurm@wur.nl>
2024-01-31 23:13:15 +0100

    added callback

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -4,43 +4,52 @@ import matplotlib.pyplot as plt
 from utils import plot_predictions
 import os
 import locationencoder.pe as pe
+import locationencoder.nn as nn
 
 def plot_weights(pl_module,
                  current_epoch,
-                 vmin=-0.5,
-                 vmax=0.5,
+                 vmin=None,
+                 vmax=None,
                  savepath=None,
                  show=False):
     L = pl_module.positional_encoder.L
-    weights_flat = pl_module.neural_network.weight.detach().numpy()[0]
+    if isinstance(pl_module.neural_network, nn.SirenNet):
+        weights_flat_all = pl_module.neural_network.layers[0].weight.detach().numpy()
+    else: # linear
+        weights_flat_all = pl_module.neural_network.weight.detach().numpy()
 
-    lm = []
-    for l in range(L):
-        for m in range(-l, l + 1):
-            lm.append((l, m))
+    for i, weights_flat in enumerate(weights_flat_all):
 
-    W = np.ones((L, 2 * L)) * np.nan
-    for (l, m), w in zip(lm, weights_flat):
-        W[l, m + L] = w
+        lm = []
+        for l in range(L):
+            for m in range(-l, l + 1):
+                lm.append((l, m))
 
-    fig, ax = plt.subplots()
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.set_xticks(np.arange(-L, L, 5)[1:] + L)
-    ax.set_xticklabels(np.arange(-L, L, 5)[1:])
-    ax.set_xlabel("order m")
-    ax.set_ylabel("degree l")
+        W = np.ones((L, 2 * L)) * np.nan
+        for (l, m), w in zip(lm, weights_flat):
+            W[l, m + L] = w
 
-    ax.imshow(W, cmap="magma", vmin=vmin, vmax=vmax)
-    ax.set_title(f"epoch {current_epoch}")
-    if show:
-        plt.show()
+        fig, ax = plt.subplots()
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.set_xticks(np.arange(-L, L, 5)[1:] + L)
+        ax.set_xticklabels(np.arange(-L, L, 5)[1:])
+        ax.set_xlabel("order m")
+        ax.set_ylabel("degree l")
 
-    if savepath is not None:
-        fig.savefig(savepath, transparent=True, bbox_inches="tight", pad_inches=0)
+        ax.imshow(W, cmap="magma", vmin=vmin, vmax=vmax)
+        ax.set_title(f"weight {i} epoch {current_epoch}")
+        if show:
+            plt.show()
 
+        if savepath is not None:
+            path, ext = os.path.splitext(savepath)
+            fig.savefig(path + f"_{i}" + ext, transparent=True, bbox_inches="tight", pad_inches=0)
+        plt.clf()
+        plt.cla()
+        plt.close(fig)
 
 
 
